ff8792b2287a00c90927c243353139b55499f266
easyliu <easyliu@tencent.com>
2023-09-16 23:46:51 +0800

    feat: use hilt

--- app/src/main/java/com/easyliu/demo/mvi/CounterViewModel.kt
+++ app/src/main/java/com/easyliu/demo/mvi/CounterViewModel.kt
@@ -1,12 +1,27 @@
 package com.easyliu.demo.mvi
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
 
 /**
  * @author easyliu
  * @date 2023/8/9 23:00
  */
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CounterUiStateModule {
+
+    @Provides
+    fun provideCounterUiState(): CounterUiState {
+        return CounterUiState()
+    }
+}
+
 @HiltViewModel
 class CounterViewModel @Inject constructor(initialState: CounterUiState) :
         BaseMviViewModel<CounterUiState, CounterUiIntent, CounterEvent>(initialState) {
